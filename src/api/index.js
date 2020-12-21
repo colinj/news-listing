803f23d57e4c8e44d7b67170476d82f502020ca2
Colin Johnsun <colin.johnsun@gmail.com>
2020-12-21 12:47:30 +1100

    refactor: Use mock API to avoid over-using real API

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,15 @@
-import axios from 'axios'
+import axios from "axios"
 
 const httpClient = axios.create({
   headers: {
-    'Content-Type': 'application/json'
+    "Content-Type": "application/json"
   }
 })
 
 const getNewsArticles = () => {
-  const url = 'https://my.api.mockaroo.com/fed-exercise-data.json'
-  const params = { key: 'cf334d90' }
+  // const url = "https://my.api.mockaroo.com/fed-exercise-data.json"
+  const url = "/articles.json"
+  const params = { key: "cf334d90" }
   return httpClient.get(url, { params })
 }
 
