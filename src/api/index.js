2f6191645ab985a10654727d802961b31e7b63e8
Colin Johnsun <colin.johnsun@gmail.com>
2020-12-21 12:43:36 +1100

    feat: Add axios and news listing api endpoint

---
+++ src/api/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+const httpClient = axios.create({
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+const getNewsArticles = () => {
+  const url = 'https://my.api.mockaroo.com/fed-exercise-data.json'
+  const params = { key: 'cf334d90' }
+  return httpClient.get(url, { params })
+}
+
+export { getNewsArticles }
